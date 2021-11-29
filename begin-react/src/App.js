8d6b892d6cf7c68ff507b4446c37ac5a60d8219b
Heesang <80827042+Heeesang@users.noreply.github.com>
2021-11-29 23:28:13 +0900

    Conditional Rendering

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <Wrapper>
       <Hello/>
-      <Hello name= "react" color="red"/>
+      <Hello name= "react" color="red" isSpecial/>
       <div style= {style}>React</div>
       <div className = 'gray-box'></div> 
 
