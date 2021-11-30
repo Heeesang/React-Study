3245b47f820624855afc5bd338532fbb86a49959
Heesang <80827042+Heeesang@users.noreply.github.com>
2021-11-30 23:25:55 +0900

    useState
    컴포넌트에서 바뀌는 값 관리 해버리기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import Hello from './Hello';
-import './App.css';
-import Wrapper from './Wrapper';
+// import Hello from './Hello';
+// import './App.css';
+// import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
-  const name = "react";
-  const style ={
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem',
-  };
+  // const name = "react";
+  // const style ={
+  //   backgroundColor: 'black',
+  //   color: 'aqua',
+  //   fontSize: 24,
+  //   padding: '1rem',
+  // };
 
   return (
-    <Wrapper>
-      <Hello/>
-      <Hello name= "react" color="red" isSpecial/>
-      <div style= {style}>React</div>
-      <div className = 'gray-box'></div> 
+    // <Wrapper>
+    //   <Hello/>
+    //   <Hello name= "react" color="red" isSpecial/>
+    //   <div style= {style}>React</div>
+    //   <div className = 'gray-box'></div> 
+    // </Wrapper>
 
-    </Wrapper>
+    <Counter/>
   );
 }
 
