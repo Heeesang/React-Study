fa0e08471effc3750eb545080ad793945b3df0af
Heesang <80827042+Heeesang@users.noreply.github.com>
2021-12-13 17:30:30 +0900

    useState

--- practice/component/src/App.js
+++ practice/component/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
 
 function App() {
   return (
-    <MyComponent name="React" favoriteNumber={2}>리액트</MyComponent>
+    <>
+      <MyComponent name="React" favoriteNumber={2}>리액트</MyComponent>
+      <Counter/>
+      <Say/>
+    </>  
   );
 }
 
