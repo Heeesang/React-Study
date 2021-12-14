49fdc33db4db72f00808a063d7cdfd3fab1c4c40
Heesang <80827042+Heeesang@users.noreply.github.com>
2021-12-14 23:48:34 +0900

    useState_prac

--- practice/component/src/Say.js
+++ practice/component/src/Say.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react' ;
+import React, { useState } from "react";
 
 const Say = () => {
-    const [message, setMessage] = useState('');
-    const onClickEnter = () => setMessage('안녕하세요!');
-    const onClickLeave = () => setMessage('안녕히 가세요!');
+  const [message, setMessage] = useState("");
+  const onClickEnter = () => setMessage("안녕하세요!");
+  const onClickLeave = () => setMessage("안녕히 가세요!");
+  return (
+    <div>
+      <button onClick={onClickEnter}>ENTER</button>
+      <button onClick={onClickLeave}>LEAVE</button>
+      <h1>{message}</h1>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <button onClick={onClickEnter}>ENTER</button>
-            <button onClick={onClickLeave}>LEAVE</button>
-            <h1>{message}</h1>
-        </div>
-    )
-}
-
-export default Say; 
+export default Say;
