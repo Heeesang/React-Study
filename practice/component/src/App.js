7cd2a6ba63e4f8a092d6e4240ba1db45c21eedff
Heesang <80827042+Heeesang@users.noreply.github.com>
2021-12-15 09:27:07 +0900

    array_map()

--- practice/component/src/App.js
+++ practice/component/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import MyComponent from './MyComponent';
-import Counter from './Counter';
-import Say from './Say';
-import EventPractice from './EventPractice';
-
+import React from "react";
+import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+import Say from "./Say";
+import EventPractice from "./EventPractice";
+import IterationSample from "./IterationSample";
 function App() {
   return (
     <>
-      <MyComponent name="React" favoriteNumber={2}>리액트</MyComponent>
-      <Counter/>
-      <Say/>
-      <EventPractice/>
-    </>  
+      <MyComponent name="React" favoriteNumber={2}>
+        리액트
+      </MyComponent>
+      <Counter />
+      <Say />
+      <EventPractice />
+      <IterationSample />
+    </>
   );
 }
 
