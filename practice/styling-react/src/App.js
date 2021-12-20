b067172b88f15187fb76f2c967c45b5df9a29476
Heesang <80827042+Heeesang@users.noreply.github.com>
2021-12-20 15:03:15 +0900

    Component_Styling

---
+++ practice/styling-react/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
+
+function App() {
+  return (
+    <div>
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
